6f77b2693df995ec68dde11979171c6c79bbe11b
Alfred Yuan <yuanxun.cn@gmail.com>
2019-02-01 21:55:32 +0800

    use pipe to synchronize producer and consumer

---
+++ app/multi-process/helloPipe.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: alfred_yuan
+# Created on 2019-02-01
+
+import multiprocessing
+
+
+def create_items(pipe):
+    output_pipe, _ = pipe
+    for item in range(10):
+        output_pipe.send(item)
+    output_pipe.close()
+
+
+def multipy_items(pipe_1, pipe_2):
+    close, input_pipe = pipe_1
+    close.close()
+    output_pipe, _ = pipe_2
+    try:
+        while True:
+            item = input_pipe.recv()
+            output_pipe.send(item * item)
+    except EOFError:
+        output_pipe.close()
+
+
+if __name__ == '__main__':
+    pipe_1 = multiprocessing.Pipe(True)
+    process_pipe_1 = multiprocessing.Process(target=create_items, args=(pipe_1,))
+    process_pipe_1.start()
+
+    pipe_2 = multiprocessing.Pipe(True)
+    process_pipe_2 = multiprocessing.Process(target=multipy_items, args=(pipe_1, pipe_2))
+    process_pipe_2.start()
+
+    pipe_1[0].close()
+    pipe_2[0].close()
+
+    try:
+        while True:
+            print(pipe_2[1].recv())
+    except EOFError:
+        print("End")
